642ad70b46656f294b8c137a15a7975f6276f363
Michael Tautonico <mario6297@gmail.com>
2023-01-14 22:43:40 -0500

    Starting over for the first time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,36 +10,7 @@ import {ProcessInputService} from './services/processInput/process-input.service
 export class AppComponent implements OnInit {
   constructor(private processInputService: ProcessInputService) {
   }
-
   translations: any;
-  combo: string = '';
-  combo_list: Array<string> = [];
-  realComboList: any = [];
-
-
-  // This portion is for translating the input
-  translateInput() {
-    for (let i = 0; i < this.realComboList.length; i++) {
-      let input = this.translations[this.realComboList[i]];
-      if (input !== undefined) {
-        this.realComboList[i][0] = [this.translations[this.realComboList[i][0]]];
-      }
-    }
-  }
-
-  processChange() {
-    this.combo_list = this.combo.split('>')
-    for (let i = 0; i < this.combo_list.length; i++) {
-      // This is to reset the list because im using .push()
-      this.realComboList = []
-      // Remove the unnecessary characters from the input
-      this.combo_list[i] = this.combo_list[i].trim();
-      console.log(this.combo_list);
-      this.realComboList.push([[this.combo_list[i]]]);
-    }
-    this.translateInput();
-    console.log(this.realComboList);
-  }
 
   async ngOnInit() {
     // Importing the input translations from the JSON file
