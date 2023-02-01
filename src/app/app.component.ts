fd506d88dbd4df9855737c56323304197a95f8b3
Michael Tautonico <mario6297@gmail.com>
2023-01-31 23:21:15 -0500

    Fuck you

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,19 +19,63 @@ export class AppComponent implements OnInit {
   getArrayLength(array: Array<any>) {
     return array.length;
   }
+
   processInput() {
     let splitInput = this.userInput.split('>')
     this.processedUserInput = [];
     for (let i = 0; i < splitInput.length; i++) {
       let input = splitInput[i].trim();
       let payload = [];
-      if (this.attackInputs.includes(input.charAt(input.length-1).toUpperCase())) {
+      let attack: string;
+      let aerial: boolean = false;
+      if (this.attackInputs.includes(input.charAt(input.length - 1).toUpperCase())) {
         payload.push(this.translations[input.slice(0, -1)]);
-        payload.push(this.translations[input.charAt(input.length-1).toUpperCase()]);
+        payload.push(this.translations[input.charAt(input.length - 1).toUpperCase()]);
       } else if (this.translations[input] !== undefined) {
         payload.push(this.translations[input]);
       } else {
-        payload.push(input);
+        if (input.length === 6 || input.length === 7) {
+          if (this.attackInputs.includes(input.charAt(input.length - 1).toUpperCase())) {
+            attack = input.charAt(input.length - 1).toUpperCase();
+            input = input.slice(0, -1);
+          }
+          if (input.charAt(0).toLowerCase() + input.charAt(1).toLowerCase() === 'j.') {
+            aerial = true;
+            input = input.slice(2);
+          }
+          switch (input) {
+            // TODO: Fix undefined error when adding an attack
+            case '632146':
+              payload.push(this.translations['63214']);
+              payload.push(this.translations['6']);
+              // @ts-ignore
+              if (attack !== '') {
+                // @ts-ignore
+                payload.push(this.translations[attack])
+              }
+              break;
+            case '236236':
+              payload.push(this.translations['236']);
+              payload.push(this.translations['236']);
+              // @ts-ignore
+              if (attack !== '') {
+                // @ts-ignore
+                payload.push(this.translations[attack])
+              }
+              break;
+            case '214214':
+              payload.push(this.translations['214']);
+              payload.push(this.translations['214']);
+              // @ts-ignore
+              if (attack !== '') {
+                // @ts-ignore
+                payload.push(this.translations[attack])
+                break;
+              }
+          }
+        } else {
+          payload.push(input);
+        }
       }
       // @ts-ignore
       this.processedUserInput.push(payload);
